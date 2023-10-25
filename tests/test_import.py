9e91f9b2fba7ab2aefa117dd3d9aa15567dd644c
Andrew Elliott <ande.elliott@gmail.com>
2023-10-25 12:44:41 +0100

    adding first set of tests

---
+++ tests/test_import.py
@@ -0,0 +1,21 @@
+
+
+
+
+def test_import_package():
+    import bufflib
+    bufflib.load_data
+    bufflib.plotting
+    bufflib.cluster
+
+
+def test_import_load_data():
+    from bufflib import load_data
+
+
+def test_import_plotting():
+    from bufflib import plotting
+
+
+def test_import_cluster():
+    from bufflib import cluster
